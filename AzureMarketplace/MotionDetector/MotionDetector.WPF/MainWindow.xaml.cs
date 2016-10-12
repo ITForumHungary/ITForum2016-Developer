3f84d599f716ce44ef96cda059409b706ac0be53
Mark Szabo <szmaorka@gmail.com>
2016-10-12 17:42:31 +0200

    styling of list of fragments

--- AzureMarketplace/MotionDetector/MotionDetector.WPF/MainWindow.xaml.cs
+++ AzureMarketplace/MotionDetector/MotionDetector.WPF/MainWindow.xaml.cs
@@ -1,116 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Windows;
-using System.Threading;
-using System.Xml.Linq;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Threading;
-using Windows.Foundation;
 using System.Windows.Controls;
+using System.Timers;
+using System.Windows.Threading;
+using System.Windows.Data;
+using System.Windows.Media;
 
 namespace MotionDetector.WPF
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    
     public partial class MainWindow : Window
     {
-        List<MotionFragment> fragments = new List<MotionFragment>();
-        Timer mainTimer;
-        MotionData data;
+        public static DispatcherTimer mainTimer;
+        public static MotionData data;
+        public static MotionData getData()
+        {
+            return data;
+        }
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void media_MediaOpened(object sender, RoutedEventArgs e)
-        {
 
             #region parse
-            //TODO: Open file (?)
-            string exampleJSON = @"{
-                'version': 2,
-                'timescale': 23976,
-                'offset': 0,
-                'framerate': 24,
-                'width': 1280,
-                'height': 720,
-                'regions': [
-                    {
-                        'id': 0,
-                        'type': 'polygon',
-                        'points': [
-                            {'x': 0, 'y': 0},
-                            {'x': 0.5, 'y': 0},
-                            {'x': 0, 'y': 1}
-                        ]
-                    }
-                ],
-                'fragments': [
-                    {
-                        'start': 0,
-                        'duration': 226765
-                    },
-                    {
-                        'start': 226765,
-                        'duration': 47952,
-                        'interval': 999,
-                        'events': [
-                            [
-                                {
-                                    'type': 2,
-                                    'typeName': 'motion',
-                                    'locations': [
-                                        {
-                                            'x': 0.004184,
-                                            'y': 0.007463,
-                                            'width': 0.991667,
-                                            'height': 0.985185
-                                        }
-                                    ],
-                                    'regionId': 0
-                                }
-                            ]
-                        ]
-                    }
-                ]
-            }";
 
             StreamReader jsonReader = new StreamReader("Resources/MI_motiondetection.json");
             String jsonData = jsonReader.ReadToEnd();
 
-            //Parse the fragments
+            //parse the fragments
             data = JsonConvert.DeserializeObject<MotionData>(jsonData);
 
             MotionEventList.ItemsSource = data.fragments;
-
-            //Parse the fragments
-            /*JObject parsedJson = JObject.Parse(exampleJSON);
-            IList<JToken> frags = parsedJson["fragments"].Children().ToList();
-            //timescale = Convert.ToInt32(parsedJson["timescale"].ToString());
-            foreach (JToken frag in frags)
-            {
-                //Deserialize to a MotionFragment object
-                MotionFragment fragment = JsonConvert.DeserializeObject<MotionFragment>(frag.ToString());
-                //Add the deserialized object to the (global) fragments list
-                fragments.Add(fragment);
-            }*/
             #endregion
-
-            //SecurityVideo.Play();
-            //mainTimer = new Timer(timer_tick, null, 0, 100);
         }
 
-        void timer_tick(Object o)
+        private void media_MediaOpened(object sender, RoutedEventArgs e)
         {
-            foreach (MotionFragment f in fragments)
+            //start the timer
+            mainTimer = new DispatcherTimer(
+                TimeSpan.FromMilliseconds(100),
+                DispatcherPriority.Normal,
+                (_,__) => Update(), 
+                this.Dispatcher);
+            
+            mainTimer.Start();
+        }
+
+        private void Update()
+        {
+            foreach (Fragment f in data.fragments)
             {
                 var start = Convert.ToInt32(f.start);
                 var end = start + Convert.ToInt32(f.duration);
@@ -120,16 +62,19 @@ namespace MotionDetector.WPF
                     if (f.events != null)
                     {
                         MotionStatus.Text = "Mozgás van";
+                        MotionStatus.Foreground = new SolidColorBrush(Colors.Red);
                         break;
                     }
                     else
                     {
                         MotionStatus.Text = "Nincs mozgás";
+                        MotionStatus.Foreground = new SolidColorBrush(Colors.Green);
                     }
                 }
                 else
                 {
                     MotionStatus.Text = "Nincs mozgás";
+                    MotionStatus.Foreground = new SolidColorBrush(Colors.Green);
                 }
             }
         }
@@ -142,5 +87,25 @@ namespace MotionDetector.WPF
                 SecurityVideo.Position = TimeSpan.FromSeconds(clickedFragment.start/data.timescale);
             }
         }
+        
     }
+    public class TicToTimeConverter : IValueConverter
+    {
+        public object Convert(object tic, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (tic is int)
+            {
+                
+                TimeSpan time = TimeSpan.FromSeconds(System.Convert.ToInt32(tic) / MainWindow.getData().timescale);
+                return time.ToString();
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+    }
+
 }
